8b1a27fd9f6f97a5cd0b1e32b92b12cd003f1f60
Ariel J <arieljakubowski@gmail.com>
2019-05-20 21:38:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-deliLine = []
+line = []
 
 def line(line)
 if(line.size == 0)
@@ -15,4 +15,14 @@ end
 def take_a_number(line, person)
   line.push(person)
   puts "(line.size). person"
+end
+
+def now_serving(line)
+  line_number = katz_deli.count
+    if line.size == 0
+      puts "There is nobody waiting to be served!"
+    else
+      puts "Currently serving #{line.first}."
+      katz_deli.shift
+    end
 end
